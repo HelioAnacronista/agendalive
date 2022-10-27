73cb10f9e207a6e4b14b539acdd15bcdde6cc6f2
Hélio Fernandes <104803451+HelioAnacronista@users.noreply.github.com>
2022-10-27 20:39:28 -0300

    Getting data from external API

--- src/app/views/home/live-list/live-list.component.ts
+++ src/app/views/home/live-list/live-list.component.ts
@@ -1,4 +1,7 @@
+import { Live } from './../../../shared/model/live.model';
+import { LiveService } from './../../../shared/service/live.service';
 import { Component, OnInit } from '@angular/core';
+import { ThisReceiver } from '@angular/compiler';
 
 @Component({
   selector: 'app-live-list',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./live-list.component.css']
 })
 export class LiveListComponent implements OnInit {
+  
+  livesPrevious!: Live[];
+  livesNext!: Live[];
 
-  constructor() { }
+  constructor(
+    public liveService : LiveService
+  ) { }
 
   ngOnInit(): void {
+    this.getLive();
+  }
+
+  getLive() {
+    this.liveService.getLivesWithflag('previous').subscribe(data => {
+      this.livesPrevious = data.content;
+      console.log(this.livesPrevious);
+    });
+
+    this.liveService.getLivesWithflag('next').subscribe(data => {
+      this.livesNext = data.content;
+      console.log(this.livesNext);
+    });
   }
 
 }
+
+  
+
